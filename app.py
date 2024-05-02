cc494465b5d2f47744ce0d2a555b7d2fe33bdb05
Ravikrishna J <139262612+call-meRavi-SHORT-CODE@users.noreply.github.com>
2024-05-02 12:56:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-
-    
-from flask import Flask , render_template , request , jsonify
+from flask import Flask, render_template, request, jsonify , send_from_directory
+import os
 from chat import get_response
 
 app = Flask(__name__)
 
-@app.route('/')
 
-def index_get():
+
+@app.route('/')
+def index():
     return render_template("base.html")
 
-@app.post("/predict")
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static/favicon.ico', '', as_static=True)
+
+
+@app.route("/predict", methods=['POST'])
 def predict():
-    text = request.get_json().get('message')
-    response = get_response(text)
-    message = {'answer' : response}
-    return jsonify(message)
-
-if __name__== "main":
-    app.run(debug=True)
-
+    try:
+        # Extract message from JSON payload
+        text = request.get_json().get('message')
+        
+        # Call chatbot function to get response
+        response = get_response(text)
+        
+        # Construct response message
+        message = {'answer': response}
+        
+        # Return response as JSON
+        return jsonify(message), 200
+    except Exception as e:
+        # Return error message if an exception occurs
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
 
 
